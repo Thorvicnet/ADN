d7eb28a598ffc92766aa8f388fde85f41a5e7944
Alexandre Lisiecki <76170880+userlex-0@users.noreply.github.com>
2022-10-16 09:11:19 +0000

    base.html + navbar + custom dna (arguments)

--- modules/DNAGeneration.py
+++ modules/DNAGeneration.py
@@ -3,8 +3,9 @@ from modules.TransTrad import gen_adn, traduction, transcription
 ADN = ['A', 'T', 'C', 'G']
 
 
-def run(brin_ADN=None):
-  if brin_ADN is None:
+def run(
+    brin_ADN=None):  # Aucune idée pourquoi des fois il lance pas avec argument
+  if brin_ADN == 'None' or brin_ADN is None:
     brin_ADN = gen_adn(ADN)
   if transcription(brin_ADN) is False:
     return run()
@@ -12,5 +13,3 @@ def run(brin_ADN=None):
     ARN_start, ARN = transcription(brin_ADN)
     GEN = traduction(ARN_start)
     return [brin_ADN, ARN, ARN_start, GEN]
-
-
