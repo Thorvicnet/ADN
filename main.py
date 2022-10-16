c92195feb91fe64da02c1651fb0001061df2f129
Thorvicnet <73116308+Thorvicnet@users.noreply.github.com>
2022-10-16 07:44:20 +0000

    some changes

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return 'Hello'
+
+
+@app.route('/results')
+def results():
   data = run()
-  return render_template('index.html', generationdata = data)
+  return render_template('results.html', generationdata=data)
 
 
 app.run(host='0.0.0.0', port=81, debug=True)
