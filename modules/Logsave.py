35344ed8d78db056456ca00a4fb32045be413102
thovicnet <thovicnet@gmail.com>
2022-10-22 12:31:40 +0200

    Added log to .gitignore and logsave will now create a new file when needed

--- modules/Logsave.py
+++ modules/Logsave.py
@@ -3,12 +3,15 @@ from datetime import datetime
 
 
 def logwrite(text):
-  f = open('adn.log', 'r')
-  logs = f.readlines()
-  f.close()
-  f = open('adn.log', 'w')
-  text = '\n\n' + datetime.fromtimestamp(
-    time.time()).strftime("%d-%m-%Y, %H:%M:%S") + ' ' + str(text)
-  logs.append(text)
-  f.writelines(logs)
-  f.close()
+  try:
+    f = open('adn.log', 'r') # On ouvre les logs et on les enregistrent
+    logs = f.readlines()
+    f.close()
+    f = open('adn.log', 'w') # On ouvre les logs et on rajoute la date et les données nécessaires
+    text = '\n\n' + datetime.fromtimestamp(
+      time.time()).strftime("%d-%m-%Y, %H:%M:%S") + ' ' + str(text)
+    logs.append(text)
+    f.writelines(logs)
+    f.close()
+  except: # Si le fichier n'existe pas : le créer ("x")
+    f = open("adn.log","x")
