5be75ad33419f523e066bb9844ac23cd99747104
Thorvicnet <73116308+Thorvicnet@users.noreply.github.com>
2022-10-26 17:13:55 +0000

    before dna script change

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,7 @@
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time)); // crée une promise pour l'async eu bout d'un temps time
+}
+
 function backgroundcanvas(mouseX, mouseY) {
     const canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth; // mettre à la taille de la fenêtre
@@ -9,7 +13,7 @@ function backgroundcanvas(mouseX, mouseY) {
     const ctx = canvas.getContext("2d");
     for (let x = 0; x<canvas.width; x+=space) {
         for (let y = 0; y<canvas.height; y+=space) {
-            let distance = Math.sqrt((mouseX-x)**2 + (mouseY-y)**2);
+            let distance = Math.sqrt((mouseX-x)**2 + (mouseY-y)**2); //calcul de la distance
             /*if (distance > 125) {
                 ctx.beginPath();
                 ctx.arc(x, y, 1, 0, 2 * Math.PI, true); // on trace les points (cercle) normalement
@@ -19,7 +23,7 @@ function backgroundcanvas(mouseX, mouseY) {
                 continue
             }
             else {
-              let mov = (mouseX/distance)
+              let mov = (1000/distance)
               //let mov = (mouseX/((1)+Math.sqrt(distance**2))) // test de fonction différente
               if (x - mouseX > 0) { // pour les différentes directions
                   newx = x + mov;
@@ -34,30 +38,52 @@ function backgroundcanvas(mouseX, mouseY) {
                   newy = y - mov;
               }
               ctx.beginPath();
-              ctx.arc(newx, newy, 1, 0, 2 * Math.PI, true); // ontrace les pints (cercle)
+              ctx.arc(newx, newy, 1, 0, 2 * Math.PI, true); // on trace les points (cercle)
               ctx.stroke();
             }
         }
     }
 }
 
+var running = false; // Pour empĉher le programme de se lancer plusieurs fois
+      
+async function spinFast() {
+  if (running == false) { // Si la onction est déjà entrain de tourner ne pas la lancer
+    running = true;
+    console.log("spinning"); //pour print dans la console
+    const object = document.querySelectorAll('.ele');
+    for (let i = 2; i > 1.5; i=i-0.01) {
+      t= 4-(i**2) // pour que l'animation deccélère plus i s'approche de sa vitesse max (i = 1)
+      object.forEach(ele => { // Même chose qu'un "for ele in object:" en python
+      animdel = ele.style['animation-delay']; // j'en ai besoin puisque la ligne suivante supprime cette valeur
+      ele.style.animation='run ' + i + 's linear infinite'; // On change la vitesse d'animation
+      ele.style['animation-delay'] = animdel;
+      });
+      await sleep(t);
+    }
+    object.forEach(ele => { // on remet à la normale
+      animdel = ele.style['animation-delay'];
+      ele.style.animation='run 2s linear infinite';
+      ele.style['animation-delay'] = animdel;
+      });
+    await sleep(1000) // laisser un temps d'attente pour qu'il ne se déclenche pas imédiatemment
+    running = false
+  }
+}
 
 window.addEventListener("DOMContentLoaded", function(){ // on attend que le DOM se charge
     document.addEventListener('mousemove', (event) => {
         backgroundcanvas(event.clientX, event.clientY);
         //console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`); // pour debug (fait lagger)
     });
+    var dna = document.querySelector('.dna');
+    dna.addEventListener("mouseover", () => {spinFast()});
 });
 
-// ne fonctionne pas encore car value est défini après que la fonction soit appelé
-function copy_gendata(){
-  var val = null  // en attendant
+
+function copy_gendata(objclass){ // prend en argument la class du texte de tag <p> et d'id = "gendata"
+  var val = document.getElementsByClassName(objclass).gendata.innerText; // on accede au text dans l'élément
    // On copie
   navigator.clipboard.writeText(val);
-
-  // Alerter l'utilisateur
-  alert("Donnée de génération copié : " + data[0]);
+  alert("C'est copié ! \n")
 }
- function test() {
-   alert('test')
- }
