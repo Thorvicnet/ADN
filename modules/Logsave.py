561146596fff8857ee855ebed02127e02dac3ed1
thovicnet <thovicnet@gmail.com>
2022-11-03 12:13:35 +0100

    added coments to Logsave and TransTrad

--- modules/Logsave.py
+++ modules/Logsave.py
@@ -3,18 +3,26 @@ from datetime import datetime
 
 
 def logwrite(text):
+  """Ecrit un texte dans les adn.log avec devant la date et heure
+
+  Args:
+      text (str): texte à écrire dans les logs
+  """
   try:
     with open('adn.log', 'r') as f:
-      logs = f.readlines()  # On ouvre les logs et on les enregistrent
+      logs = f.readlines()  # On ouvre les logs et on les écrits dnas logs
     f = open(
       'adn.log', 'w'
-    )  # On ouvre les logs et on rajoute la date et les données nécessaires
+    )  # On ouvre les logs en mode d'écriture
     text = '\n\n' + datetime.fromtimestamp(
-      time.time()).strftime("%d-%m-%Y, %H:%M:%S") + ' ' + str(text)
-    logs.insert(0, text)
-    f.writelines(logs)
-    f.close()
+      time.time()).strftime("%d-%m-%Y, %H:%M:%S") + ' ' + str(text) # On rajoute au texte la date et heure
+    # time.time() nous donne la time stamp
+    # datetime.fromtimestamp() la traduit en valeur compréhensible
+    # strftime(format) la transforme dans le format voulu, ici "day-month-year, hour:minutes:seconds"
+    logs.insert(0, text) # On rajoute la nouvelle entrée dans les logs
+    f.writelines(logs) # On écrit les logs au fichier
+    f.close() # On ferme le fichier
   except:  # Si le fichier n'existe pas : le créer ("x")
     f = open("adn.log", "x")
     f.close()
-    logwrite(text)
+    logwrite(text) # Et on réessaye
--- modules/TransTrad.py
+++ modules/TransTrad.py
@@ -9,14 +9,31 @@ ERRORS LIST:
 3 = erreur dans RNA_DNA() : le format de l'ARNm entré n'existe pas
 """
 
+RANDOMSIZE = 100 # Permet de modifier la taille de la string générée par randomDNA() et randomRNA()
+
+
 
 def tochunk(m):
+  """Transforme un texte ou une liste en groude de 3 de ces éléments
+
+  Args:
+      m (str or list): éléments à fragmenter en groupe de 3
+
+  Returns:
+      list: la liste contenant les éléments 3 par 3
+  """
   return [(m[x] + m[x + 1] + m[x + 2]) for x in range(0, len(m) - 2, 3)]
 
 
-def stringifyGEN(
-  GEN
-):  # Permet d'enlever la liste pour la remplacer par les protéines séparées par des virgules
+def stringifyGEN(GEN):  # Permet d'enlever la liste pour la remplacer par les protéines séparées par des virgules
+  """Transforme une liste en string séparant par des virgules les différents éléments
+
+  Args:
+      GEN (list): éléments à rassembler en string
+
+  Returns:
+      str: éléments dans une string séparés par des virgules
+  """
   try:
     return ", ".join(GEN)
   except:  # Si GEN est vide
@@ -25,12 +42,12 @@ def stringifyGEN(
 
 def randomDNA():
   DNA = ['A', 'T', 'C', 'G']
-  return "".join([DNA[randint(0, len(DNA) - 1)] for _ in range(100)])
+  return "".join([DNA[randint(0, len(DNA) - 1)] for _ in range(RANDOMSIZE)])
 
 
 def randomRNA():
   RNA = ['G', 'C', 'A', 'U']
-  return "".join([RNA[randint(0, len(RNA) - 1)] for _ in range(100)])
+  return "".join([RNA[randint(0, len(RNA) - 1)] for _ in range(RANDOMSIZE)])
 
 
 def DNA_RNA(DNA):
@@ -50,23 +67,29 @@ def RNA_DNA(RNA):
 
 
 def RNA_GEN(staticRNA):
+  """Trouve les acides aminés dans l'ARNm
+
+  Args:
+      staticRNA (str): ARNm d'où nous voulons trouver les acides aminés
+
+  Returns:
+      list: liste contenant les acides aminés
+  """
   RNAlist = []
-  for frame in range(
-      3
-  ):  # Cherche pour chaque alignement, par exemple "AAUGA" aura une recherche sur ['AAU','GA'], ['AUG','A'] et ['UGA']
+  for frame in range(3):  # Cherche pour chaque alignement, par exemple "AAUGA" aura une recherche sur ['AAU','GA'], ['AUG','A'] et ['UGA']
     RNA = staticRNA[frame:]  # on décale en supprimant les premières valeurs
     RNA = tochunk(RNA)
-    dict = dictGEN.trad2GEN()
-    RNA = [dict[x] for x in RNA]
-    startPOS = [x for x in range(len(RNA)) if RNA[x] == 'Met']
-    endPOS = [x for x in range(len(RNA)) if RNA[x] == 'STO']
+    dict = dictGEN.trad2GEN() # On récupère l'énorme dictionnaire
+    RNA = [dict[x] for x in RNA] # On fait la traduction avec l'ARNm
+    startPOS = [x for x in range(len(RNA)) if RNA[x] == 'Met'] # Positions de tous les codons-start
+    endPOS = [x for x in range(len(RNA)) if RNA[x] == 'STO'] # Positions de tous les codons-stop
 
     maxend = -1
     for start in startPOS:
       if maxend < start:  # Pour empêcher d'avoir des acides aminés dans des acides aminés (AUGAUGUGA)
         for end in endPOS:
           if end > start:
-            RNAlist.append("".join(RNA[start:end]))
+            RNAlist.append("".join(RNA[start:end])) # On à trouver un acide aminés de pos "start" à "stop" que l'on enregistre dans RNAlist
             maxend = end
             break
   if RNAlist == []:  # Rien n'a été trouvé : erreur
@@ -80,6 +103,16 @@ def DNA_GEN(DNA):
 
 
 def autoData(value, type):
+  """Fournit directement quoi afficher sur la page à partir de la valeur et si elle est DNA ou RNA
+     si value = 'None' alors elle sera générée aléatoirement
+
+  Args:
+      value (str): la string à traduire et transcrire
+      type (str): le type de value fournit (soit 'DNA' ou 'RNA')
+
+  Returns:
+      list: liste contenant [ADN, ARNm, acide aminés]
+  """
   try:
     if type == 'DNA':
       DNA = value
@@ -95,6 +128,6 @@ def autoData(value, type):
       data[2] = 'Aucune protéine complète trouvée'
     log.logwrite(data)  
   except:
-    log.logwrite('Did not work')
-    data = None
+    log.logwrite('Did not work') # L'utilisateur à entrer une valeur impossible
+    data = None # il sera redirigé sur / avec un message d'alerte
   return data
